0e4f5b5778bbaa29c5fe850b9421a47ab5f88947
MohamedKadir252 <mohamed.abdulkadir855@gmail.com>
2024-12-28 21:05:45 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 def main():
     book_path = "books/frankenstein.txt"
-    with open(book_path) as f:
-        file_contents = f.read()
-        words = get_num_words(file_contents)
-        num_charecter = get_num_charecter_dict(file_contents)
-        print_report(words, num_charecter, book_path)
+    text = get_book_text(book_path)
+    words = get_num_words(text)
+    num_charecter = get_num_charecter_dict(text)
+
+    #print report
+    print_report(words, num_charecter, book_path)
 
 
 def print_report(nb_words, nb_chars_dict, b_path):
-    print(f"--- Bigin report of {b_path} ---")
+    print(f"--- Begin report of {b_path} ---")
     print(f"{nb_words} words found in the document")
-    for key in nb_chars_dict:
-        print(f"the '{key}' character was found {nb_chars_dict[key]} times")
+    print()
+
+    for key in sorted(nb_chars_dict.items(), key=lambda x : x[1], reverse=True):
+        print(f"the '{key[0]}' character was found {key[1]} times")
+
     print("--- End report ---")
 
 
+def get_book_text(path):
+    with open(path) as f:
+        lecture = f.read()
+    return lecture
+
+
+def get_num_words(string):
+    words = string.split()
+    return len(words)
+
+
+#get a dict of {"char": appear n time in text}
 def get_num_charecter_dict(string):
     char_num ={}
     for item in string:
@@ -25,11 +41,6 @@ def get_num_charecter_dict(string):
             else:
                 char_num[lower] = 1     
     return char_num
-
-
-def get_num_words(string):
-    words = string.split()
-    return len(words)
 
 
 if __name__ == "__main__":
